3508d8a2716b3d3b59e0267c7190d3a50b269afa
shahloxon qutbiddinova <shahloxonqutbiddinova@gmail.com>
2025-08-27 20:53:31 +0500

    1-bosqich

---
+++ vazifa-ilova/ilova.py
@@ -0,0 +1,8 @@
+def menyu():
+    print("===MENYU===")
+    print("1 - Vazifa qo‘shish")
+    print("2 - Ro‘yxatni ko‘rish")
+    print("3 - Vazifani o‘chirish")
+    print("0 - Chiqish")
+
+menyu()
